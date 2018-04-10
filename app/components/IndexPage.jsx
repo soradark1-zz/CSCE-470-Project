0ddc2d29618c644b80fef80ebc850cd813648bf8
unknown <soradark1@gmail.com>
2018-04-10 16:22:43 -0500

    testing fetch url

--- app/components/IndexPage.jsx
+++ app/components/IndexPage.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import PathLoader from 'path-loader'
 
 export default class IndexPage extends React.Component {
+
+  componentDidMount(){
+    fetch('https://facebook.github.io/react-native/movies.json')
+   .then((response) => response.json())
+   .then((responseJson) => {
+     console.log( responseJson.movies);
+   })
+  }
+
   render() {
     return (
       <h1>Home Page</h1>
