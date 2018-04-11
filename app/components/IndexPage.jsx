38e23b111e4bb40eeb4019cd925c8c4dbcf97a66
Clifton Sims <soradark1@gmail.com>
2018-04-11 00:24:19 -0500

    added dumby links on IndexPage

--- app/components/IndexPage.jsx
+++ app/components/IndexPage.jsx
@@ -1,21 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class IndexPage extends React.Component {
-
-  componentDidMount(){
-    fetch('http://localhost:8983/solr/ps4_games/select?fl=title&q=title:%22Kingdom%22')
-   .then((response) => response.json())
-   .then((responseJson) => {
-     console.log( responseJson);
-   })
-   .catch((error) => {
-      console.error(error);
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      myjson: {},
+      titles: []
+    };
   }
 
+
+  componentDidMount(){
+    return fetch('http://localhost:8983/solr/ps4_games/select?q=title:%22Kingdom%22')
+     .then((response) => response.json())
+     .then((responseJson) => {
+       console.log( responseJson.response.docs[0]);
+       let titles = this.getTitles(responseJson.response.docs)
+       this.setState({
+         myjson: responseJson.response.docs,
+         titles: titles
+       });
+       return responseJson
+     })
+     .catch((error) => {
+        console.error(error);
+      });
+  }
+
+
+
+  renderTitleLinks(){
+    const listItems = this.state.titles.map((title) =>
+      <Link to="/">{title}</Link>
+    );
+    return listItems;
+  }
+
+  getTitles(docs){
+    let titles = []
+    for (var i = 0; i < 5; i++) {
+      titles.push(docs[i].title[0]);
+    }
+    return titles;
+  }
+
+
+
   render() {
+
     return (
+      <div>
       <h1>Home Page</h1>
+      {console.log(this.state.titles)}
+      {this.renderTitleLinks()}
+      </div>
     );
   }
 }
