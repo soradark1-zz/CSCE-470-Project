7f3828b53799309e455d2b05c2f6191e26dd7ed9
jiang50 <jiang50@tamu.edu>
2018-04-23 16:25:11 -0500

    Add files via upload

--- app/components/GamePage.jsx
+++ app/components/GamePage.jsx
@@ -6,7 +6,7 @@ export default class GamePage extends React.Component {
     super(props);
     this.state = {
       title: [],
-      reviews: []
+      reviews: [],
     }
   }
 
@@ -36,7 +36,68 @@ export default class GamePage extends React.Component {
     );
     return listItems;
   }
+  
+  renderscore(resultscore){
+	  console.log(resultscore);
+	  return <div><br/>{"Sentiment:"} {resultscore}</div>
+  }
+  
+  rendersentiment(){
+	'use strict';
 
+	let https = require ('https');
+
+
+	let accessKey = 'a19ee40ac1674a068e6325b8034055d6';
+	var reviews = this.state.reviews;
+	var reviewtext = "";
+	var resultscore = 0;
+	reviews.map((reviews, i) => {
+		if((i+1) % 4 == 0 && i < 40){
+		   reviewtext += reviews;
+		}
+	  }
+	);
+	
+	const requestBody = {
+		  "documents": [
+			{
+			  "language": "en",
+			  "id": "1",
+			  "text": reviewtext,
+			}
+		  ]
+		};
+		
+	
+		  const request = new XMLHttpRequest();
+		  request.open('POST', 'https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment', true);
+		  request.setRequestHeader("Content-Type", "application/json");
+		  request.setRequestHeader("Ocp-Apim-Subscription-Key", "a19ee40ac1674a068e6325b8034055d6"); // Set API key.
+		  request.setRequestHeader("Accept","application/json");
+		  request.onload = () => {
+			let body_ = JSON.parse (request.responseText);
+			let body__ = JSON.stringify (body_, null, '  ');
+			
+			console.log (body__);
+			if(JSON.parse(request.responseText).documents.length !=0){
+				
+				resultscore = JSON.parse(request.responseText).documents[0].score*100;
+				console.log(resultscore);
+				this.renderscore(resultscore);
+//				return <div><br/>{"Sentiment:"} {resultscore}</div>
+			}
+		  };
+		  request.onerror = () => {
+			alert(request.responseText);
+		  };
+		  request.send(JSON.stringify(requestBody));
+
+
+  }
+  
+  
+ 
 
 
   render() {
@@ -44,8 +105,8 @@ export default class GamePage extends React.Component {
     return (
       <div>
       <h1>{this.state.title}</h1>
+	  {this.rendersentiment()}	  
       {this.renderReviews()}
-
       </div>
     );
   }
